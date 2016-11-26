35b63f46c09ddb2f8541853a13d102736b5ba41a
CalebGM <calebmayerski@cox.net>
2016-11-25 21:46:47 -0800

    python now prints out all indexes of rows with missing shot values

--- Part1/loadData.py
+++ Part1/loadData.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+
+
 df = pd.read_csv(r'../data.csv')
-stuff = df.shot_made_flag
-print(stuff)
+shot_attempt = df.shot_made_flag
+for index, shot in enumerate(shot_attempt):
+	if np.isnan(shot):
+		print(index)
